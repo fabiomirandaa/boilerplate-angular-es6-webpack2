5f1581f9773a19a79a7e6738db577950e411d239
Gustavo da Cruz <gecruz.dev@gmail.com>
2017-06-21 19:28:14 -0300

    refactor: change deprecated ng-annotate to babel angularjs-annotate explicit mode

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,15 +1,12 @@
-import './home.view.html';
-import '../../images/coringa.jpg';
+import './home.view.html'
+import '../../images/coringa.jpg'
 
-class HomeController {
-    constructor($http, $scope) {
-        this._$http = $http;
-        this._$scope = $scope;
-    }
+export class HomeController {
+  constructor ($http, $scope) {
+    'ngInject'
 
+    this._$http = $http
+    this._$scope = $scope
+  }
 
 }
-
-HomeController.$inject = ['$http', '$scope'];
-
-export { HomeController };
--- app/router.js
+++ app/router.js
@@ -1,14 +1,16 @@
-class Router {
-    static configure(app) {
-        app.config(($stateProvider, $urlRouterProvider) => {
-            $urlRouterProvider.otherwise('/');
-            $stateProvider
-                .state('main', {
-                    url: '',
-                    templateUrl: 'components/home/home.view.html',
-                    controller: 'HomeController as ctrl'
-                })
-                });
-        }
-    }
-export { Router };
+export class Router {
+  static configure (app) {
+    app.config(($stateProvider, $urlRouterProvider) => {
+      'ngInject'
+
+      $urlRouterProvider.otherwise('/')
+
+      $stateProvider
+        .state('main', {
+          url: '',
+          templateUrl: 'components/home/home.view.html',
+          controller: 'HomeController as ctrl'
+        })
+    })
+  }
+}
